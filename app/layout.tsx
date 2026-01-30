79bab8bf76b1b9d891a76fd54a65f15f4b76eea3
SymphonyIceAttack <1994370030@qq.com>
2026-01-30 10:07:07 +0800

    feat: rebuild

--- app/layout.tsx
+++ app/layout.tsx
@@ -142,7 +142,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head></head>
+      <head>
+        <script
+          src="https://quge5.com/88/tag.min.js"
+          data-zone="206815"
+          async
+          data-cfasync="false"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
       >
--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
   domain: "3nbf4.com",
-  zoneId: 10540074,
+  zoneId: 10540080,
 };
 self.lary = "";
 importScripts("https://3nbf4.com/act/files/service-worker.min.js?r=sw");
