b4f3acc675fe8a09fd6ca700a567babac80e3bb1
mouraddi <94260095+mouraddi@users.noreply.github.com>
2026-07-28 02:20:46 +0100

    add /pinterest to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -49,6 +49,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.7,
     },
     {
+      url: `${baseUrl}/pinterest`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.6,
+    },
+    {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: "monthly",
